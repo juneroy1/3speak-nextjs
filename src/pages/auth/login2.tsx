a7ac56d27e96575a5535657a261d3108a2e182ec
Juneroy Quinimon <eroyjune@gmail.com>
2023-03-07 02:23:27 +0800

    feat: added signup tabs -- DONE

--- src/pages/auth/login2.tsx
+++ src/pages/auth/login2.tsx
@@ -31,6 +31,9 @@ const TabsDemo = () => {
           <Tabs.Trigger className="TabsTrigger" value="tab2">
             Sign Up with Hive Account
           </Tabs.Trigger>
+          <Tabs.Trigger className="TabsTrigger" value="tab3">
+            Sign Up
+          </Tabs.Trigger>
         </Tabs.List>
         <Tabs.Content className="TabsContent" value="tab1">
           <Box width="100%">
@@ -223,6 +226,166 @@ const TabsDemo = () => {
                 <StyledButton type="submit">Sign Up</StyledButton>
               </Flex>
             </Box>
+          </Box>
+        </Tabs.Content>
+        <Tabs.Content className="TabsContent" value="tab3">
+          <Box width="100%">
+            <Box mx="auto" maxWidth="9rem">
+              <img
+                src="https://s3.eu-central-1.wasabisys.com/data.int/logo_player.png"
+                alt="3speak logo"
+                width="100%"
+              />
+            </Box>
+            <Box
+              width="100%"
+              borderRadius="0.25rem"
+              mt="1.5rem"
+              py="0.75rem"
+              px="1.25rem"
+              backgroundColor="#f8d7da"
+              border="1px solid #f5c6cb"
+            >
+              <Typography textAlign="center" color="#721c24">
+                Some email services will wrongly sort our our emails, remember
+                to check your junk/spam folder!
+              </Typography>
+            </Box>
+            <Box
+              width="100%"
+              borderRadius="0.25rem"
+              mt="1.5rem"
+              py="0.75rem"
+              px="1.25rem"
+              backgroundColor="#f8d7da"
+              border="1px solid #f5c6cb"
+            >
+              <Typography textAlign="center" color="#721c24">
+                In order to claim the keys for the Hive account associated with
+                your 3speak account you must post at least one video.
+              </Typography>
+            </Box>
+            <Formik
+              initialValues={{ password: "", email: "" }}
+              validate={(props) => {
+                const errors: any = {};
+
+                if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(props.email)) {
+                  errors.email = t("login.notValidEmail");
+                }
+
+                if (!props.password) errors.password = t("required");
+                if (!props.email) errors.email = t("required");
+
+                return errors;
+              }}
+              onSubmit={(values) => {
+                console.log(values, "submit");
+              }}
+            >
+              {(props) => (
+                <form onSubmit={props.handleSubmit}>
+                  <Box mb="2rem" mt="1.5rem" width="100%">
+                    <fieldset className="Fieldset">
+                      <label className="Label" htmlFor="currentPassword">
+                        Email
+                      </label>
+                      <input
+                        className="Input"
+                        id="email"
+                        placeholder={t("login.email")}
+                        type="email"
+                        onChange={props.handleChange}
+                        onBlur={props.handleBlur}
+                        name="email"
+                      />
+                      {!!props.errors.email && (
+                        <Typography color="#FF3333">
+                          {props.errors.email}
+                        </Typography>
+                      )}
+                    </fieldset>
+                    {/* <StyledInput
+                    type="string"
+                    className="Input"
+                    placeholder={t("login.email")}
+                    onChange={props.handleChange}
+                    onBlur={props.handleBlur}
+                    error={!!props.errors.email}
+                    name="email"
+                    id="email"
+                  /> */}
+                  </Box>
+                  <Box width="100%">
+                    <fieldset className="Fieldset">
+                      <label className="Label" htmlFor="username">
+                        Hive username
+                      </label>
+                      <input
+                        className="Input"
+                        id="username"
+                        placeholder="hive username"
+                        type="text"
+                      />
+                    </fieldset>
+                  </Box>
+                  <Box width="100%">
+                    {/* <StyledInput
+                    type="password"
+                    className="Input"
+                    placeholder={t("login.password")}
+                    onChange={props.handleChange}
+                    onBlur={props.handleBlur}
+                    error={!!props.errors.password}
+                    name="password"
+                    id="password"
+                  /> */}
+                    <fieldset className="Fieldset">
+                      <label className="Label" htmlFor="newPassword">
+                        Password
+                      </label>
+                      <input
+                        type="password"
+                        className="Input"
+                        placeholder={t("login.password")}
+                        onChange={props.handleChange}
+                        onBlur={props.handleBlur}
+                        name="password"
+                        id="password"
+                      />
+                      {!!props.errors.password && (
+                        <Typography mt="0.25rem" color="#FF3333">
+                          {props.errors.password}
+                        </Typography>
+                      )}
+                    </fieldset>
+                    <Box
+                      width="100%"
+                      borderRadius="0.25rem"
+                      mt="1.5rem"
+                      py="0.75rem"
+                      px="1.25rem"
+                      backgroundColor="#bee5eb"
+                      border="1px solid #bee5eb"
+                    >
+                      <Typography color="#0c5460">
+                        {/* fontSize="0.75rem" */}
+                        <StyledList>
+                          {(t("register.passwordRules") as string[]).map(
+                            (rule) => (
+                              <li key={rule}>{rule}</li>
+                            )
+                          )}
+                        </StyledList>
+                      </Typography>
+                    </Box>
+                  </Box>
+                  <Flex width="100%" justifyContent="center" mt="1rem">
+                    <StyledButton type="submit">Sign Up</StyledButton>
+                  </Flex>
+                </form>
+              )}
+            </Formik>
           </Box>
         </Tabs.Content>
       </Tabs.Root>
