b8fc811f969e63e53a9c1c7ea1845392997ab10d
Juneroy Quinimon <eroyjune@gmail.com>
2023-05-11 22:27:19 +0800

    feat: fixed issues in running build --- DONE

--- src/components/signup/SignUp.tsx
+++ src/components/signup/SignUp.tsx
@@ -7,7 +7,7 @@ import { Formik } from "formik";
 import { useRouter } from "next/router";
 import { useTranslation } from "next-export-i18n";
 import { Typography, Box, Flex } from "src/components";
-import ReCAPTCHA from "react-google-recaptcha";
+// import ReCAPTCHA from "react-google-recaptcha";
 const SignUp = () => {
   const router = useRouter();
   const dispatch = useDispatch();
