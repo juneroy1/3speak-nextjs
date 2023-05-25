feae541c07cfd0037baf686d2e8ed90e0bcc29cf
Juneroy quinimon <eroyjune@gmail.com>
2023-05-25 19:35:34 +0800

    feat: added user page

--- next.config.js
+++ next.config.js
@@ -2,11 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["images.hive.blog",'api.lorem.space', "picsum.photos","placeimg.com"],
+    domains: ["images.hive.blog",'api.lorem.space', "picsum.photos","placeimg.com", "threespeakvideo.b-cdn.net"],
     remotePatterns: [
       {
         protocol: "https",
         hostname: "images.hive.blog",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "threespeakvideo.b-cdn.net",
         port: "",
         pathname: "/**",
       },
