0aaf7ed12b881e0ff7fd87869448b23816a00b45
Juneroy Quinimon <eroyjune@gmail.com>
2023-06-06 22:49:48 +0800

    feat: adjust fonts size added padding and added border black

--- src/pages/new2.tsx
+++ src/pages/new2.tsx
@@ -141,11 +141,11 @@ const New2 = () => {
         {/* {videos.length} */}
         {videos.map((video, index) => (
           <GridItem w="100%" h="100%" key={index}>
-            <Image alt="test" src={`${video.thumbnail}`} />
-            <Text fontWeight={"bold"} marginY={"10px"} as="h3">
+            <Image padding={'5px'} backgroundColor={'#222 !important'} alt="test" src={`${video.thumbnail}`} />
+            <Text fontSize={'13px'} fontWeight={"bold"} marginY={"10px"} as="h5">
               {video.title}
             </Text>
-            <Text fontWeight={"bold"} marginTop={"10px"} as="h3">
+            <Text fontWeight={"bold"} marginTop={"10px"} as="span">
               {video.username}
             </Text>
             <Text as="p">a day ago</Text>
